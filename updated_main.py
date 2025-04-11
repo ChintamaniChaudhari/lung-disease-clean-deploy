08c3377333b8c4b1aa70fcb4c7773e3040f1f4e0
ChintamaniChaudhari <chintamanichaudhari123@gmail.com>
2025-04-11 12:34:18 +0530

    Update updated_main.py

--- updated_main.py
+++ updated_main.py
@@ -41,44 +41,44 @@ set_background("BackGround.jpg")
 with st.container():
     st.markdown("<div class='block-container'>", unsafe_allow_html=True)
 
-# set title
-st.title('Multiple Lung Disease classification')
-
-# set header
-st.header('Please upload a chest X-ray image')
-
-# upload file
-file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
-
-# load classifier
-model = load_model("converted_keras/keras_model.h5")
-
-# load class names
-with open("converted_keras/keras_model.h5", 'r') as f:
-    class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
-    f.close()
-
-# display image
-if file is not None:
-    image = Image.open(file).convert('RGB')
-    st.image(image, use_column_width=True)
-
-    # classify image
-    class_name, conf_score, all_preds = classify(image, model, class_names)
-
-    st.subheader("Output")
-
-    # write classification
-    st.write("## {}".format(class_name))
-    st.write("### score: {}%".format(int(conf_score * 1000) / 10))
-    #class_name, conf_score, all_preds = classify(image, model, class_names)
-
-    # show predicted label
-    st.write("### Predicted: `{}` with {:.2f}% confidence".format(class_name, conf_score * 100))
-
-    # display horizontal bar chart like your image
+    # set title
+    st.title('Multiple Lung Disease classification')
     
-    for i, score in enumerate(all_preds):
-        st.markdown(f"**{class_names[i]}**")
-        st.progress(float(score))
-    st.markdown("</div>", unsafe_allow_html=True)
+    # set header
+    st.header('Please upload a chest X-ray image')
+    
+    # upload file
+    file = st.file_uploader('', type=['jpeg', 'jpg', 'png'])
+    
+    # load classifier
+    model = load_model("converted_keras/keras_model.h5")
+    
+    # load class names
+    with open("converted_keras/keras_model.h5", 'r') as f:
+        class_names = [a[:-1].split(' ')[1] for a in f.readlines()]
+        f.close()
+    
+    # display image
+    if file is not None:
+        image = Image.open(file).convert('RGB')
+        st.image(image, use_column_width=True)
+    
+        # classify image
+        class_name, conf_score, all_preds = classify(image, model, class_names)
+    
+        st.subheader("Output")
+    
+        # write classification
+        st.write("## {}".format(class_name))
+        st.write("### score: {}%".format(int(conf_score * 1000) / 10))
+        #class_name, conf_score, all_preds = classify(image, model, class_names)
+    
+        # show predicted label
+        st.write("### Predicted: `{}` with {:.2f}% confidence".format(class_name, conf_score * 100))
+    
+        # display horizontal bar chart like your image
+        
+        for i, score in enumerate(all_preds):
+            st.markdown(f"**{class_names[i]}**")
+            st.progress(float(score))
+        st.markdown("</div>", unsafe_allow_html=True)
